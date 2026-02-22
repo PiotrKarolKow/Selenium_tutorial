8dc6160f9d2b3a7beb7d556e1ff14a5867a1c8cb
PiotrKarolKow <piotr.kowalczyk1210@gmail.com>
2026-02-22 12:14:35 +0000

    kroki testowe

---
+++ datatools.py
@@ -0,0 +1,11 @@
+class Gender:
+    MALE = 0
+    FEMALE = 1
+
+class TestData:
+    EMAIL = "marcin@marcin.com"
+    GENDER = Gender.FEMALE
+    LAST_NAME = "Nowak"
+    VALID_PASSWORD = "sdfsdfsdfsd"
+
+print(f"ZAIMPORTOWANO {__name__}")
